0658ca28010cfe14801528de20c9476eb16f8044
Adrián <145448579+aadrii09@users.noreply.github.com>
2025-03-10 15:59:49 +0100

    estilos navbar y footer hechos

--- retro-rescue/src/components/Footer.jsx
+++ retro-rescue/src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-gray-800 text-white p-4 text-center mt-4'>
+      <p>&copy;{new Date().getFullYear()} Mi tienda - Todos los derechos reservados</p>
+    </footer>
   )
 }
 
--- retro-rescue/src/components/Navbar.jsx
+++ retro-rescue/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+ return (
+   <nav className='bg-gray-800 text-white p-4 flex justify-between'>
+     <Link to='/' className='text-white text-lg font-bold'>Mi tienda</Link>
+     <div>
+       <Link to='/products' className='mx-2'>Productos</Link>
+       <Link to='/cart' className='mx-2'>Carrito</Link>
+       <Link to='/login' className='mx-2'>Iniciar sesión</Link>
+       <Link to='/register' className='mx-2'>Registrarse</Link>
+     </div>
+   </nav>
+ )
 }
 
 export default Navbar
