66824052aeadebddddf9b00107149559c7fe673c
Adrián <145448579+aadrii09@users.noreply.github.com>
2025-03-11 16:38:55 +0100

    ya podemos añadir productos

--- retro-rescue/src/pages/Products.jsx
+++ retro-rescue/src/pages/Products.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from 'react'
+import { getProducts } from '../services/productService'
+import { Link } from 'react-router-dom'
+
 const Products = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try { 
+        const data = await getProducts()
+        setProducts(data)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    fetchData();
+  }, [])
+
   return (
-    <div>Products</div>
+    <div className='nes-container with-title m-5'>
+      <h2 className='title'>Catálogo de Consolas Retro</h2>
+      <div className='nes-container is-dark flex flex-wrap gap-4'>
+        {products.length > 0 ? products.map(product => (
+          <div key={product.id} className='nes-container is-rounded is-dark'>
+            <p>{product.name}</p>
+            <p>{product.price}</p>
+            <Link to={`/product/${product.id}`} className='nes-btn is-primary'>Ver detalles</Link>
+          </div>
+        )) : <p className='nes-text is-error'>Cargando productos...</p>}
+      </div>
+    </div>
   )
 }
 
---
+++ retro-rescue/src/services/productService.js
@@ -0,0 +1,11 @@
+import {db} from './firebaseConfig';
+import {collection, getDoc , getDocs} from 'firebase/firestore';
+
+export const getProducts = async () => {
+    try {
+        const querySnapshot = await getDocs(collection(db, 'products'));
+        return querySnapshot.docs.map(doc =>({id: doc.id, ...doc.data}));
+    } catch (error) {
+        throw new Error('Error al obtener los productos' + error.message);
+    }
+} 
